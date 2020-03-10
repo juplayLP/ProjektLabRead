08debe02cbf3e5b1eace8c2ba5aa681be46d19a9
juplaylp <jonstro@web.de>
2020-03-10 22:32:11 +0100

    Work

--- ProjektLabRead/Login.cs
+++ ProjektLabRead/Login.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+using WinDruckauftrag;
 
 namespace ProjektLabRead
 {
     class Login
     {
+        Dbase db =new Dbase("Projektlabor","root","");
+        public Login(string user, string Pass)
+        {
+
+        }
+
+        public Login(string rootkey)
+        {
+
+        }
     }
 }
