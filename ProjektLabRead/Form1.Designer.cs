aa30f37946f0a80044139a565909955972c951d5
juplaylp <jonstro@web.de>
2020-03-11 07:35:04 +0100

    Status

--- ProjektLabRead/Form1.Designer.cs
+++ ProjektLabRead/Form1.Designer.cs
@@ -141,7 +141,7 @@
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "PLock Manager";
+            this.Text = "PLock Manager | Login";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
