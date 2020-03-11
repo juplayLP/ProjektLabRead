f07ccdb9a88eb38e91459a99f0c285f71e9f11f4
AmbossLover <amboss.lover69@gmail.com>
2020-03-11 08:58:44 +0100

    verwaltung Fenster

---
+++ ProjektLabRead/Verwaltung.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ProjektLabRead
+{
+    partial class Verwaltung
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Tab = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.Zugriffdata = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Tab.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Zugriffdata)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Tab
+            // 
+            this.Tab.Controls.Add(this.tabPage1);
+            this.Tab.Controls.Add(this.tabPage2);
+            this.Tab.Location = new System.Drawing.Point(0, 0);
+            this.Tab.Name = "Tab";
+            this.Tab.SelectedIndex = 0;
+            this.Tab.Size = new System.Drawing.Size(798, 448);
+            this.Tab.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(790, 419);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "User";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.Zugriffdata);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(790, 419);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Zugriffsrechte";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Zugriffdata
+            // 
+            this.Zugriffdata.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Zugriffdata.Location = new System.Drawing.Point(3, 6);
+            this.Zugriffdata.Name = "Zugriffdata";
+            this.Zugriffdata.RowHeadersWidth = 51;
+            this.Zugriffdata.RowTemplate.Height = 24;
+            this.Zugriffdata.Size = new System.Drawing.Size(782, 405);
+            this.Zugriffdata.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(784, 413);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Verwaltung
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Tab);
+            this.Name = "Verwaltung";
+            this.Text = "Verwaltung";
+            this.Tab.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Zugriffdata)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl Tab;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView Zugriffdata;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ ProjektLabRead/Verwaltung.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinDruckauftrag;
+
+namespace ProjektLabRead
+{
+    public partial class Verwaltung : Form
+    {
+        public Verwaltung()
+        {
+            InitializeComponent();
+            Dbase dtbl = new Dbase("Projektlabor", "root", "");
+            dtbl.CommandSelectAsDataTableFrom("User");
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
